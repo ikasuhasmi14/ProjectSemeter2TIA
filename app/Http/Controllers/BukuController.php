f2533172450aa0d53531d47f124759183d38667d
ikasuhasmi14 <ikasuhasmi@alumni.pcr.ac.id>
2023-12-05 09:50:23 +0700

    Versi 1

---
+++ app/Http/Controllers/BukuController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Buku;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class BukuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $buku = Buku::latest()->paginate(10);
+        return view('buku.index', compact('buku'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Buku  $buku
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Buku $buku)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Buku  $buku
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Buku $buku)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Buku  $buku
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Buku $buku)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Buku  $buku
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Buku $buku)
+    {
+        //
+    }
+}
